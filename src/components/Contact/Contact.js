84ff04512e464067c7ed09763adbfaa616e10b5d
CaxaROCK <yakubueric@gmail.com>
2022-07-15 12:11:41 -0400

    modified submit form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Container, SectionTitle } from "../StyledComponents";
 import { ContactContainer, ContactInfo, ContactOption, ContactForm, ContactEmail } from "./styles";
 import { RiMailSendFill } from "react-icons/ri";
@@ -8,12 +8,13 @@ import { homeAnimation, useScroll } from "../Animations";
 import emailjs from "emailjs-com";
 
 function Contact() {
+    const formRef = useRef();
     const [element, animatonControls] = useScroll();
     const sendEmail = (e) => {
         e.preventDefault();
         emailjs.sendForm("service_h9ugz1g", "template_q7vjsvb", e.target, "hPzqgJtE0w5VJyj1k");
-        // .then((res) => console.log(res))
-        // .catch((err) => console.log(err));
+        alert("Thank you for reaching out to me, I'll contact you back as soon as possible!");
+        formRef.current.reset();
     };
     return (
         <Container variants={homeAnimation} ref={element} animate={animatonControls} initial="hidden" id="contact">
@@ -25,7 +26,7 @@ function Contact() {
                     <ContactOption href="#">Uxbridge, ON</ContactOption>
                 </ContactInfo>
                 <ContactEmail variants={homeAnimation}>Email me</ContactEmail>
-                <ContactForm variants={homeAnimation} onSubmit={sendEmail}>
+                <ContactForm variants={homeAnimation} onSubmit={sendEmail} ref={formRef}>
                     <div>
                         <div className="formInputSmall">
                             <input name="user_name" type="text" placeholder="Name" />
